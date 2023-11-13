c6450da7248d1a6751b025da95c7cefd9b2930ca
Pepito Mercurio <coranthin.etheve@ynov.com>
2023-11-13 13:47:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,110 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todolist.ui.theme.ToDoListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoListTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var input by remember { mutableStateOf("") }
+    var toDoList by remember { mutableStateOf(emptyArray<String>())}
+
+    Column (modifier = Modifier.fillMaxSize()) {
+        for (elem in toDoList) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    text = elem,
+                    modifier = modifier
+                )
+
+                Button(onClick = {
+                    toDoList = toDoList.filterNot { it == elem }.toTypedArray()
+                },
+                    modifier = Modifier
+                        .background(Color.Red)
+                ) {
+                    Text(text = "-")
+                }
+            }
+
+        }
+
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            TextField(
+                value = input,
+                onValueChange = {newText -> input = newText },
+                singleLine = true,
+                label = {
+                    Text(text = "Ajoutez une tache...")
+                }
+            )
+            Button(onClick = {
+                if (input != "") {
+                    toDoList += input
+                    input = ""
+                }
+            },
+                modifier = Modifier
+                    .background(Color.Green)
+            ) {
+                Text(text = "+")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ToDoListTheme {
+        Greeting("Android")
+    }
+}
